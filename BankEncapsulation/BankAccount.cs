20fa84969d4b0b67dee8ef81f51dd70189223a6a
mark aldridge <mark.aldridge1@protonmail.com>
2024-04-30 11:05:38 -0700

    Banking excercise check-in

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,17 @@
+﻿namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+        public void Deposit(double dep)
+        {
+            Console.WriteLine($"Starting balance is ${balance}");
+            balance += dep;
+            Console.WriteLine($"After depositing ${dep}, new balance is ${balance}");
+        }
+        public double GetBalance()
+        {
+            return balance;
+        }
+    }
+}
--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace BankEncapsulation
+﻿using System.ComponentModel.Design;
+
+namespace BankEncapsulation
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            string selection = "";
+            int amount = 0;
+            BankAccount account = new BankAccount();
+            do
+            {
+                Console.WriteLine("Press 1 to see account balance");
+                Console.WriteLine("Press 2 to make a deposit");
+                Console.WriteLine("Press 3 to exit");
+                selection = Console.ReadLine();
+                switch (selection)
+                {
+                    case "1":
+                        Console.WriteLine("Account balance is $" + account.GetBalance());
+                        Console.WriteLine();
+                        break;
+                    case "2":
+                        Console.WriteLine("Enter deposit amount");
+                        amount = int.Parse(Console.ReadLine());
+                        account.Deposit(amount);
+                        Console.WriteLine();
+                        break;
+                    case "3":
+                        break;
+                }
+
+            } while (selection != "3");
+
         }
     }
 }
